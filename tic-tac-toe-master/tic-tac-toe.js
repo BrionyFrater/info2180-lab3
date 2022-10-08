aeee93526d6b6dacf50373dce0c70f2f7874c15f
Briony Frater <briefrater@gmail.com>
2022-10-08 15:25:25 -0500

    Added X/O on click

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,11 +1,31 @@
 window.onload = function(){
 
+    let gamePieces = [""];
     let squareDivs = document.getElementById("board").querySelectorAll("div");
     
     for(const item of squareDivs){
         item.className = "square";
     }
-       
+
+    for(const item of squareDivs){
+        item.addEventListener("click", function(){
+            if(gamePieces[gamePieces.length - 1] == "O" || gamePieces[gamePieces.length - 1] == ""){
+
+                item.classList.add("X");
+                item.textContent = "X";
+                gamePieces.push("X");
+                
+            }
+            else if(gamePieces[gamePieces.length - 1] == "X"){
+
+                item.classList.add("O");
+                item.textContent = "O";
+                gamePieces.push("O");
+                
+            }
+        });
+    }
+
 }
 
 

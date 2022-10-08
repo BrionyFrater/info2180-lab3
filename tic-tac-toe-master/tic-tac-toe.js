c3b4ca9e58664f9f0a506397c3f1e8751fa24fa0
Briony Frater <briefrater@gmail.com>
2022-10-08 10:05:43 -0500

    Revealed squares

---
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+    let squareDivs = document.getElementById("board").querySelectorAll("div");
+    
+    for(const item of squareDivs){
+        item.className = "square";
+    }
+       
+}
+
+
+
+
+
+
